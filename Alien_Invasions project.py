c89031826bb64c93a1f63eca5e18920d27a707b7
jonnybarca <alexander1dlc@gmail.com>
2018-05-29 08:12:27 -0400

    alien invaders video game - finished/ish product

--- Alien_Invasions project.py
+++ Alien_Invasions project.py
@@ -2,9 +2,12 @@
 import sys
 
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
+import game_functions as gf
 
 
 def run_game():
@@ -15,16 +18,21 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
-    # Make a ship.
-    ship = Ship(screen)
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
+
 
     # Start the main loop for the game.
     while True:
-
-        # Watch for keyboard and mouse events.
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
         # Redraw the screen during each pass through the loop.
         screen.fill(ai_settings.bg_color)
@@ -35,5 +43,3 @@ def run_game():
 
 
 run_game()
-
-
--- settings.py
+++ settings.py
@@ -6,4 +6,15 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        self.bg_color = (0, 0, 0)
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 255, 255, 0
+        self.bullets_allowed = 16
+
